18ec6ad1f29fd75ed3b7c875bde8a9f804012c0d
Tyrian <thomasjosephward@gmail.com>
2023-10-27 21:22:51 +0100

    Creation
    Creation

---
+++ DeHazardifier.cs
@@ -0,0 +1,201 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using Comfort.Common;
+using EFT;
+using EFT.Interactive;
+using HazardPatches;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TYR_DeHazardifier
+{
+    [BepInPlugin("com.TYR.DeHazardifier", "TYR_DeHazardifier", "1.0.0")]
+    public class DeClutter : BaseUnityPlugin
+    {
+        private static GameWorld gameWorld;
+        public static bool MapLoaded() => Singleton<GameWorld>.Instantiated;
+        public static List<GameObject> savedDirectionalMinesObjects = new List<GameObject>();
+        public static List<GameObject> savedBarbedWireObjects = new List<GameObject>();
+        public static ConfigEntry<bool> deHazardifierEnabledConfig;
+        public static ConfigEntry<bool> minefieldsEnabledConfig;
+        public static ConfigEntry<bool> directionalMinesEnabledConfig;
+        public static ConfigEntry<bool> directionalMinesVisualsEnabledConfig;
+        public static ConfigEntry<bool> barbedWireEnabledConfig;
+        public static ConfigEntry<bool> barbedWireVisualsEnabledConfig;
+        public static ConfigEntry<bool> sniperBorderZonesEnabledConfig;
+        public static bool deHazardifiered = false;
+
+        private void Awake()
+        {
+            deHazardifierEnabledConfig = Config.Bind("A - De-Hazardifier Enabler", "A - De-Hazardifier Enabled", true, "Enables the De-Hazardifier.");
+            minefieldsEnabledConfig = Config.Bind("A - De-Hazardifier Settings", "A - Minefield Disabler", true, "Disables minefields.");
+            directionalMinesEnabledConfig = Config.Bind("A - De-Hazardifier Settings", "B - Claymore Mines Disabler", true, "Disables claymore mines.");
+            directionalMinesVisualsEnabledConfig = Config.Bind("A - De-Hazardifier Settings", "C - Claymore Mines Visuals Disabler", true, "Disables visual model of claymore mines.");
+            barbedWireEnabledConfig = Config.Bind("A - De-Hazardifier Settings", "D - Barbed Wire Disabler", true, "Disables barbed wire.");
+            barbedWireVisualsEnabledConfig = Config.Bind("A - De-Hazardifier Settings", "E - Barbed Wire Visuals Disabler", true, "Disables visual model of barbed wire.");
+            sniperBorderZonesEnabledConfig = Config.Bind("A - De-Hazardifier Settings", "F - Sniper Border Zones Disabler", true, "Disables sniper border zones.");
+            deHazardifierEnabledConfig.SettingChanged += OnApplyDeHazardifierSettingChanged;
+            minefieldsEnabledConfig.SettingChanged += OnApplyDeHazardifierSettingChanged;
+            directionalMinesEnabledConfig.SettingChanged += OnApplyDeHazardifierSettingChanged;
+            directionalMinesVisualsEnabledConfig.SettingChanged += OnApplyDirectionalMinesVisualsSettingChanged;
+            barbedWireEnabledConfig.SettingChanged += OnApplyDeHazardifierSettingChanged;
+            barbedWireVisualsEnabledConfig.SettingChanged += OnApplyBarbedWireVisualsSettingChanged;
+            sniperBorderZonesEnabledConfig.SettingChanged += OnApplyDeHazardifierSettingChanged;
+            SceneManager.sceneUnloaded += OnSceneUnloaded;
+            PatchSetter();
+        }
+        private void OnApplyDirectionalMinesVisualsSettingChanged(object sender, EventArgs e)
+        {
+            DirectionalMinesVisualsScene();
+        }
+        private void OnApplyBarbedWireVisualsSettingChanged(object sender, EventArgs e)
+        {
+            BarbedWireVisualsScene();
+        }
+        private void BarbedWireVisualsScene()
+        {;
+            foreach (GameObject obj in savedBarbedWireObjects)
+            {
+                if (obj != null)
+                {
+                    if (barbedWireVisualsEnabledConfig.Value)
+                    {
+                        obj.SetActive(false);
+                    }
+                    else
+                    {
+                        obj.SetActive(true);
+                    }
+                }
+            }
+        }
+        private void OnApplyDeHazardifierSettingChanged(object sender, EventArgs e)
+        {
+            PatchSetter();
+        }
+        private void DirectionalMinesVisualsScene()
+        {
+            foreach (GameObject obj in savedDirectionalMinesObjects)
+            {
+                if (obj != null)
+                {
+                    if (directionalMinesVisualsEnabledConfig.Value)
+                    {
+                        obj.SetActive(false);
+                    }
+                    else
+                    {
+                        obj.SetActive(true);
+                    }
+                }
+            }
+        }
+        private void OnSceneUnloaded(Scene scene)
+        {
+            savedDirectionalMinesObjects.Clear();
+            savedBarbedWireObjects.Clear();
+            deHazardifiered = false;
+        }
+        private void Update()
+        {
+            if (!MapLoaded() || deHazardifiered || !deHazardifierEnabledConfig.Value)
+                return;
+
+            gameWorld = Singleton<GameWorld>.Instance;
+            if (gameWorld == null || gameWorld.MainPlayer == null)
+                return;
+
+            DeHazardifyVisuals();
+            deHazardifiered = true;
+        }
+        private void DeHazardifyVisuals()
+        {
+            List<GameObject> allGameObjects = new List<GameObject>();
+            GameObject[] rootObjects = FindObjectsOfType<GameObject>();
+
+            foreach (GameObject root in rootObjects)
+            {
+                bool isMine = root.GetComponent<MineDirectional>() != null;
+                string isMineGrouped = root.name.ToLower();
+                bool isBarbedWire = root.GetComponent<BarbedWire>() != null;
+                if (directionalMinesVisualsEnabledConfig.Value && (isMine || isMineGrouped == "Mines"))
+                {
+                    allGameObjects.Add(root);
+                    savedDirectionalMinesObjects.Add(root);
+                }
+                if (barbedWireVisualsEnabledConfig.Value && isBarbedWire)
+                {
+                    allGameObjects.Add(root);
+                    savedBarbedWireObjects.Add(root);
+                }
+            }
+            DirectionalMinesVisualsScene();
+            BarbedWireVisualsScene();
+        }
+        public static void PatchSetter()
+        {
+            if (minefieldsEnabledConfig.Value)
+            {
+                new MinefieldTriggerPatch().Enable();
+                new MinefieldCoroutinePatch().Enable();
+                new MinefieldDamagePatch().Enable();
+                new MinefieldViewTriggerPatch().Enable();
+            }
+            else
+            {
+                new MinefieldTriggerPatch().Disable();
+                new MinefieldCoroutinePatch().Disable();
+                new MinefieldDamagePatch().Disable();
+                new MinefieldViewTriggerPatch().Disable();
+            }
+            if (directionalMinesEnabledConfig.Value)
+            {
+                new MineDirectionalAwakePatch().Enable();
+                new MineDirectionalTriggerPatch().Enable();
+                new MineDirectionalTriggerColliderPatch().Enable();
+                new MineDirectionalDamagePatch().Enable();
+            }
+            else
+            {
+                new MineDirectionalAwakePatch().Disable();
+                new MineDirectionalTriggerPatch().Disable();
+                new MineDirectionalTriggerColliderPatch().Disable();
+                new MineDirectionalDamagePatch().Disable();
+            }
+            if (barbedWireEnabledConfig.Value)
+            {
+                new BarbedWireDamagePatch().Enable();
+                new BarbedWireSpeedPenaltyPatch().Enable();
+                new BarbedWireSpeedPenalty2Patch().Enable();
+            }
+            else
+            {
+                new BarbedWireDamagePatch().Disable();
+                new BarbedWireSpeedPenaltyPatch().Disable();
+                new BarbedWireSpeedPenalty2Patch().Disable();
+            }
+            if (sniperBorderZonesEnabledConfig.Value)
+            {
+                new SniperImitatorAwakePatch().Enable();
+                new SniperImitatorDamagePatch().Enable();
+                new SniperImitatorShootPatch().Enable();
+                new SniperFiringZoneShootPatch().Enable();
+                new SniperFiringZoneCoroutinePatch().Enable();
+                new SniperFiringZoneTargetPatch().Enable();
+                new SniperFiringZoneTarget2Patch().Enable();
+            }
+            else
+            {
+                new SniperImitatorAwakePatch().Disable();
+                new SniperImitatorDamagePatch().Disable();
+                new SniperImitatorShootPatch().Disable();
+                new SniperFiringZoneShootPatch().Disable();
+                new SniperFiringZoneCoroutinePatch().Disable();
+                new SniperFiringZoneTargetPatch().Disable();
+                new SniperFiringZoneTarget2Patch().Disable();
+            }
+        }
+    }
+}
---
+++ Patches.cs
@@ -0,0 +1,247 @@
+﻿using Aki.Reflection.Patching;
+using EFT.Interactive;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace HazardPatches
+{
+    public class MinefieldTriggerPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(Minefield), "IsInTriggerZone");
+        }
+
+        [PatchPrefix]
+        public static bool PatchPrefix()
+        {
+            return false;
+        }
+    }
+
+    public class MinefieldCoroutinePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(Minefield), "FireCoroutine");
+        }
+
+        [PatchPrefix]
+        public static bool PatchPrefix()
+        {
+            return false;
+        }
+    }
+
+    public class MinefieldDamagePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(Minefield), "method_3");
+        }
+
+        [PatchPrefix]
+        public static bool PatchPrefix()
+        {
+            return false;
+        }
+    }
+
+    public class MinefieldViewTriggerPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(MinefieldView), "method_0");
+        }
+
+        [PatchPrefix]
+        public static bool PatchPrefix()
+        {
+            return false;
+        }
+    }
+    public class MineDirectionalAwakePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(MineDirectional), "Awake");
+        }
+
+        [PatchPrefix]
+        public static bool PatchPrefix()
+        {
+            return false;
+        }
+    }
+    public class MineDirectionalTriggerPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(MineDirectional), "OnTriggerEnter");
+        }
+
+        [PatchPrefix]
+        public static bool PatchPrefix()
+        {
+            return false;
+        }
+    }
+    public class MineDirectionalTriggerColliderPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(MineDirectional), "method_1");
+        }
+
+        [PatchPrefix]
+        public static bool PatchPrefix()
+        {
+            return false;
+        }
+    }
+
+    public class MineDirectionalDamagePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(MineDirectional), "method_3");
+        }
+
+        [PatchPrefix]
+        public static bool PatchPrefix()
+        {
+            return false;
+        }
+    }
+    public class BarbedWireDamagePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(BarbedWire), "ProceedDamage");
+        }
+
+        [PatchPrefix]
+        public static bool PatchPrefix()
+        {
+            return false;
+        }
+    }
+    public class BarbedWireSpeedPenaltyPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(BarbedWire), "AddPenalty");
+        }
+
+        [PatchPrefix]
+        public static bool PatchPrefix()
+        {
+            return false;
+        }
+    }
+    public class BarbedWireSpeedPenalty2Patch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(BarbedWire), "RemovePenalty");
+        }
+
+        [PatchPrefix]
+        public static bool PatchPrefix()
+        {
+            return false;
+        }
+    }
+    public class SniperImitatorAwakePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(SniperImitator), "Awake");
+        }
+
+        [PatchPrefix]
+        public static bool PatchPrefix()
+        {
+            return false;
+        }
+    }
+    public class SniperImitatorDamagePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(SniperImitator), "method_1");
+        }
+
+        [PatchPrefix]
+        public static bool PatchPrefix()
+        {
+            return false;
+        }
+    }
+    public class SniperImitatorShootPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(SniperImitator), "method_0");
+        }
+
+        [PatchPrefix]
+        public static bool PatchPrefix()
+        {
+            return false;
+        }
+    }
+    public class SniperFiringZoneShootPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(SniperFiringZone), "Shoot");
+        }
+
+        [PatchPrefix]
+        public static bool PatchPrefix()
+        {
+            return false;
+        }
+    }
+    public class SniperFiringZoneCoroutinePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(SniperFiringZone), "FireCoroutine");
+        }
+
+        [PatchPrefix]
+        public static bool PatchPrefix()
+        {
+            return false;
+        }
+    }
+    public class SniperFiringZoneTargetPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(SniperFiringZone), "method_3");
+        }
+
+        [PatchPrefix]
+        public static bool PatchPrefix()
+        {
+            return false;
+        }
+    }
+    public class SniperFiringZoneTarget2Patch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(SniperFiringZone), "method_4");
+        }
+
+        [PatchPrefix]
+        public static bool PatchPrefix()
+        {
+            return false;
+        }
+    }
+}
